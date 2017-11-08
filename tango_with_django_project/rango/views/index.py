0cd3fb77dfc92184ee7678f235db408e1c7a4feb
Erick Fachi <fachi1997@gmail.com>
2017-11-08 10:45:19 -0200

    Cookies de sessão no servidor (visitas ao site)

--- tango_with_django_project/rango/views/index.py
+++ tango_with_django_project/rango/views/index.py
@@ -1,12 +1,36 @@
+from datetime import datetime
 from django.shortcuts import render
 from ..models import Category, Page
 
-def index(request):
-    context_dict = {}
-    category_list = Category.objects.order_by('-likes')[:5]
-    context_dict['categories'] = category_list
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
 
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,
+                                               'last_visit',
+                                               str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).seconds > 5:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+    request.session['visits'] = visits
+
+def index(request):
+    category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-    context_dict['pages'] = page_list
+
+    visitor_cookie_handler(request)
+    context_dict = {'categories': category_list,
+                    'pages': page_list,
+                    'visits': request.session['visits'] }
+    # print(request.session['visits'], request.session['last_visit'])
 
     return render(request, 'rango/index.html', context_dict)
